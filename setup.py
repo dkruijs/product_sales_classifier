bf2f4525f9774420b0bbc173df681189489d3f78
Daan Kruijs <daan@227media.local>
2020-03-05 09:35:58 +0100

    First commit: data science cookiecutter project structure and first notebook for random forest model.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Using a Kaggle dataset, we build a model to classify inventory products to keep selling or not; focus is on building an end-to-end product for demonstration.',
+    author='227 Data Scientists',
+    license='',
+)
